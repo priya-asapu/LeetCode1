fca2887ba26c44f4633ee545f1e7724bead88452
priya-asapu <asapupriya6@gmail.com>
2025-08-30 12:24:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.7 MB (100.00%)

--- 0263-ugly-number/solution.py
+++ 0263-ugly-number/solution.py
@@ -3,6 +3,6 @@ class Solution:
         if n==0:
             return False
         for prime in 2,3,5:
-            while n%prime==0:
-                n//=prime
+             while n%prime==0:
+                 n//=prime
         return n==1
